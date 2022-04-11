fd4f530be04a1cf36f9db0d2b721d90f23ced1cc
Murali I <indukurimkv@gmail.com>
2022-04-11 17:51:55 -0400

    Add training

--- Program/main.py
+++ Program/main.py
@@ -1,17 +1,27 @@
 import DataGenerator as DG
 import SkeletonNueralNet as SNN
 import numpy as np
+import time
 
-def forward(input, weights):
+def forward(inputs, weights):
+    layerOuts = []
     for i in weights:
-        input = np.dot(input, i)
+        inputs = np.dot(inputs, i)
+        layerOuts.append(inputs)
         
-    return input
+    return inputs, layerOuts
 
 if __name__ == "__main__":
-    netIn, target = DG.getData()
-    weights = SNN.genSNN(2,1,1)
-    netOut = forward(netIn, weights)
-    
-    weights
+    n = 0.9
+    weights = SNN.genSNN(2,1)
+
+    for i in range(50):
+        netIn, target = DG.getData()
+        netOut, layerOuts = forward(netIn, weights)
+        delta0 = netOut*(1-netOut)*(target - netOut)
+        weights[-1]  += n * delta0 * layerOuts[0]
+        weights[0][0] +=n * netIn[1] * layerOuts[0] * (1-layerOuts[0]) * delta0
+        weights[0][1] +=n * netIn[0] * layerOuts[0] * (1-layerOuts[0]) * delta0
+        print(weights)
+        print(np.square(target-netOut))
     
